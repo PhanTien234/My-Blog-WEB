0a8aa2b0becc25fea0f36d1c1c44978cea9ba0c3
tranbaonguyen <tranbaonguyen0712@gmail.com>
2022-06-20 07:09:46 -0500

    add posts

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -15,10 +15,72 @@ class Post
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private$id;
 
-    public function getId(): ?int
+    /**
+     *@ORM\Column(type="string",length=255)
+     */
+
+    private$title;
+    /**
+     *@ORM\Column(type="text")
+     */
+
+    private$content;
+    /**
+     *@ORM\Column(type="datetime")
+     */
+    private$createdAt;
+
+    public function getId():?int
     {
-        return $this->id;
+        return$this->id;
+    }
+
+    /**
+     *@return mixed
+     */
+    public function getTitle()
+    {
+        return$this->title;
+    }
+
+    /**
+     *@param mixed$title
+     */
+    public function setTitle($title):void
+    {
+        $this->title=$title;
+    }
+
+    /**
+    *@return mixed
+    **/
+    public function getContent()
+    {
+        return$this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content =$content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createAt
+     */
+    public function setCreateAt($createAt): void
+    {
+        $this->createAt=$createAt;
     }
 }
