d99bff7635ebd7b7c978d3f2f5dc6b618ee6156b
PhanTien234 <minhtienp328@gmail.com>
2022-06-21 10:37:58 +0700

    create Category Table

---
+++ src/Entity/Category.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column (type="string", length=255)
+     */
+    private $title;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+}
