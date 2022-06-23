528a4f1ea2e0d420352f915e6e3fa0b3e7df1d00
PhanTien234 <minhtienp328@gmail.com>
2022-06-23 10:00:06 +0700

    test data in database

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,11 @@ class PostController extends AbstractController
     /**
      * @Route("/", name="app_post")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+        dump($posts);
+        die();
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
         ]);
