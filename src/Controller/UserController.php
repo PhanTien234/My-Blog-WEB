62a910f85c2d99a8ece9438c7a53c819284e9ecc
tranbaonguyen <tranbaonguyen0712@gmail.com>
2022-06-28 09:10:37 -0500

    creat UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="app_user")
+     */
+    public function index()
+    {
+        return $this->render('user/index.html.twig');
+    }
+}
